d2dc851ba68e5f5a413db86f5c909bc9a7a736cf
Anna <106876837+AnnaL9@users.noreply.github.com>
2022-11-21 14:52:49 -0500

    Added comments, new CSV files, Truck class, and added delivery status to Package class.

--- Main.py
+++ Main.py
@@ -1,30 +1,54 @@
 import csv
 from Package import *
+from HashTable import *
+
+# opens the Distance CSV file
+with open("CSV/Distance.csv") as distance_file:
+    distance_data = csv.reader(distance_file)
+    distance_data = list(distance_data)
+
+# # opens the Packages CSV file
+# with open("CSV/Packages.csv") as packages_file:
+#     packages_data = csv.reader(packages_file)
+#     packages_data = list(packages_data)
+
+# opens the Addresses CSV file
+with open("CSV/Addresses.csv") as Address_file:
+    Address_data = csv.reader(Address_file)
+    Address_data = list(Address_data)
 
 
+# Reads the csv file and puts the data into the hash table by package
 def load_package_data(csv_file):
     with open(csv_file) as packages:
-        read_data = csv.reader(packages, delimiter=',')
-        next(read_data)  # skip header
-        package_table = HashTable()  # create table
+        read_data = csv.reader(packages,
+                               delimiter=',')  # reads csv data with comma as the delimiter between attributes
+        package_table = HashTable()  # creates table
 
+        # Loops through each package in the csv file by its index and assigns its attributes
         for package in read_data:
             package_ID = package[0]
             package_address = package[1]
             package_city = package[2]
-            package_state = package[3]
             package_zip_code = package[4]
             package_deadline = package[5]
             package_weight_kilos = package[6]
+            package_delivery_status = "at the hub"  # uses this status as the default status
 
-            # object
-            package_test = Package(package_ID, package_address, package_city, package_state, package_zip_code,
-                                   package_deadline, package_weight_kilos)
+            # creates an object for the package
+            package_object = Package(package_ID, package_address, package_city, package_zip_code,
+                                     package_deadline, package_weight_kilos, package_delivery_status)
 
-            # insert it into the hash table
-            package_table.insert(package_ID, package_test)
+            # insert the object into the hash table
+            package_table.insert(package_ID, package_object)
 
-            HashTable.print(package_table, package_test.ID)
+            # # prints list of all packages in the hash table
+            # HashTable.print(package_table, package_object.ID)
 
 
-load_package_data('Packages.csv')
+# passes Packages.csv file into the load_pacakge_data method
+load_package_data('CSV/Packages.csv')
+
+
+
+
--- Package.py
+++ Package.py
@@ -1,18 +1,15 @@
-import csv
-from HashTable import *
-
-
+# Creates class for package
 class Package:
     # Package constructor
-    def __init__(self, ID, address, city, state, zip_code, deadline, weight_kilos):
+    def __init__(self, ID, address, city, zip_code, deadline, weight_kilos, delivery_status):
         self.ID = ID
         self.address = address
         self.city = city
-        self.state = state
         self.zip_code = zip_code
         self.deadline = deadline
         self.weight_kilos = weight_kilos
+        self.delivery_status = delivery_status
 
     def __str__(self):
-        return "%s, %s, %s, %s, %s, %s, %s" % (self.ID, self.address, self.city, self.state, self.zip_code,
-                                               self.deadline, self.weight_kilos)
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.ID, self.address, self.city, self.zip_code,
+                                               self.deadline, self.weight_kilos, self.delivery_status)
